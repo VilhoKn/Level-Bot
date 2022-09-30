a2340fc5c3b4044627869423cb1cf2cfd3c4bff9
unknown <knuutila.vilho@gmail.com>
2022-09-30 22:24:17 +0300

    Added dashboard folder and updated help command

--- src/code/commands/help.js
+++ src/code/commands/help.js
@@ -8,18 +8,50 @@ module.exports = {
 		// Create the embed
 		const embed = new Discord.MessageEmbed()
 
-		const desc = "TESTING"
+		
+
+		
+
+		// 
+		// 	.setColor(0x0099FF)
+		// 	.setTitle('Some title')
+		// 	.setURL('https://discord.js.org/')
+		// 	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+		// 	.setDescription('Some description here')
+		// 	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+		// 	.addFields(
+		// 		{ name: 'Regular field title', value: 'Some value here' },
+		// 		{ name: '\u200B', value: '\u200B' },
+		// 		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		// 		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		// 	)
+		// 	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+		// 	.setImage('https://i.imgur.com/AfFp7pu.png')
+		// 	.setTimestamp()
+		// 	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
 		// Configuring the embed
+		
+		
+		
+		const title = "Easy Level Monitoring with Level Bot"
+		const desc = "TESTING"
+
 		interaction.reply({
 			embeds: [
 				embed
-				.setTitle("TESTING")
-				.setDescription(desc)
+				.setTitle(title)
+				.setAuthor({ name: 'Level Bot', iconURL: client.user.avatarURL(), url: 'https://level.bot' })
+				//.setDescription(desc)
 				.addFields(
-					{name: 'w', value: "w" },
-					{name: 'w', value: "w" },
+					{ name: 'Setup', value: 'Setup your server with level monitoring in the Level Bot dashboard', inline: true },
+					{ name: 'Start Monitoring', value: 'Start to monitor user levels and configure level awards', inline: true },
+					{ name: 'Moderate', value: 'Add new awards along the way and build a strong community', inline: true },
+					//{ name: 'w', value: 'w', inline: true },
+					//{ name: 'w', value: 'w', inline: true }, // </kick:0>
 				)
+				.setColor(0x2f3136)
+				
 			], ephemeral: true,
 		})
 	},
